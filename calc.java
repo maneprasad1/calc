5aab280d25502d8c58742a2ad72e4ec28d7ed36d
prasad <prasadmane2003@gmail.com>
2025-04-03 15:08:45 +0530

    addition program added

---
+++ calc.java
@@ -0,0 +1,12 @@
+Class Main{
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter two numbers");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		//for addition
+		int add = a+b;
+		System.out.println("Addition : "+add);
+	}
+	}
